32de1c750c97c69ba45a4a6b8bc7bb76bc8f1af7
botgaolutgiamcan <thienbao.kd@gmail.com>
2017-08-05 22:02:08 +0700

    Delete bannerbotthapcoc.js

--- bannerbotthapcoc.js
+++
@@ -1,9 +0,0 @@
-👍👍Bột gạo lứt là thực phẩm dùng để giảm cân an toàn và hiệu quả👍👍
-💯Đảm bảo nguyên chất 
-💯Không tác dụng phụ
- (y) Phù hợp sử dụng với mọi lứa tuổi
-Hỗ trợ điều trị : tim mạch , tiểu đường , gan nhiễm mỡ , thoái hoá khớp
-📞Liên hệ ( zalo , fb ) mua Bột gạo lứt : 0909772193
-💢💢Phân phối và cung cấp Bột gạo lứt toàn quốc
-
-
